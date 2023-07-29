2ebdf0f43903f13444b16ad7b9c0c0960ef51699
artm22es <artm22es@gmail.com>
2023-07-29 09:01:49 +0200

    task-09-update

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const color = document.querySelector('.color')
+const btn = document.querySelector('.change-color')
+
+btn.addEventListener('click', handlerColor)
+
+function handlerColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+color.textContent = document.body.style.backgroundColor
+}
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
