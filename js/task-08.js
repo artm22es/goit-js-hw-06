fafce23814f22ea7612353a761928e7384d15b34
artm22es <artm22es@gmail.com>
2023-07-29 08:49:39 +0200

    task-08-update

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.login-form')
 
+form.addEventListener('submit', handlerSub)
+
+function handlerSub(event) {
+    event.preventDefault();
+
+    const { email, password } = event.currentTarget.elements
+    
+    if (!email.value || !password.value) {
+        alert("Заповніть всі поля!")
+    } else {
+        console.log(email.value);
+        console.log(password.value);
+        form.reset()
+    }
+}
